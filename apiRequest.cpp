d476dd410a3d071b446c6d73806bd09d85c56c6d
Tselmeg Otgonbayar <tselmeg.rim@gmail.com>
2026-01-13 17:44:34 +1100

    Got a working readThread with a start and stop function, and periodic data addition, need to add unique data thought

--- apiRequest.cpp
+++ apiRequest.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
 #include "nlohmann/json.hpp"
 
 #define CPPHTTPLIB_OPENSSL_SUPPORT
@@ -13,54 +14,110 @@
 #define API_REQUESTS_PER_MIN 7
 #define API_REQUEST_INTERVAL_SEC 60 / API_REQUESTS_PER_MIN + 1
 
-void readData(std::vector<double>& data, std::mutex& dataLock, int& toStop) {
-    auto client = httplib::Client{"https://api.twelvedata.com"};
-    auto MY_API_KEY = std::string{std::getenv("TWELVEDATA_MY_API_KEY")};
-    auto dataTypeRequested = std::string{"/price"};
-    auto symbol = std::string{"EUR/USD"};
+class ReadDataThread {
+private:
+    std::mutex stopSignalLock_;
+    std::condition_variable cvSignalManager_;
+    bool stopReading_;
+    std::thread thread_;
 
-    auto endPoint {dataTypeRequested + "?symbol=" + symbol + "&apikey=" + MY_API_KEY};
+    std::vector<double>& dataContainer_;
+    std::mutex& dataContainerMutex_;
 
-    auto startTime = std::chrono::high_resolution_clock::now();
-    auto apiRequestInterval = std::chrono::seconds(API_REQUEST_INTERVAL_SEC);
+    httplib::Client apiClient_;
+    std::string apiRequestEndPoint_;
 
-    while (toStop != 0) {
-        auto res = httplib::Result{client.Get(endPoint)};
+public:
+    ReadDataThread(std::vector<double>& data, std::mutex& dataLock)
+        : stopReading_ {false}
+        , dataContainer_ {data}
+        , dataContainerMutex_ {dataLock}
+        , apiClient_{"https://api.twelvedata.com"}
+    {
+        auto MY_API_KEY = std::string{std::getenv("TWELVEDATA_MY_API_KEY")};
+        auto dataTypeRequested = std::string{"/price"};
+        auto symbol = std::string{"EUR/USD"};
+
+        apiRequestEndPoint_ = {dataTypeRequested + "?symbol=" + symbol + "&apikey=" + MY_API_KEY};
+    }
+
+    auto startThread() -> void {
+        thread_ = std::thread{&ReadDataThread::readLoop, this};
+    }
+
+    auto stopThread() -> void {
+        {
+            auto stopFlagLock = std::lock_guard<std::mutex>{stopSignalLock_};
+            stopReading_ = true;
+        }
+        cvSignalManager_.notify_one();
+        thread_.join();
+    }
+
+private:
+    auto getDataRequest() -> double {
+        auto res = httplib::Result{apiClient_.Get(apiRequestEndPoint_)};
         if (res->status != 200) {
             std::cout << "status not 200\n";
         }
-        // std::cout << res->body << '\n';
 
         nlohmann::json data = nlohmann::json::parse(res->body);
 
         auto iterToPrice = data.find("price");
         if (iterToPrice != data.end()) {
             double price = std::stod(static_cast<std::string>(iterToPrice.value()));
-            std::cout << "got price: " << price << '\n';
+            return price;
         } else {
             std::cout << "no price found\n";
+            return -1;
         }
-
-        startTime += apiRequestInterval;
-        std::this_thread::sleep_until(startTime);
     }
-}
+
+    auto readLoop() -> void {
+        auto apiRequestInterval = std::chrono::duration<double>{
+            std::chrono::seconds{API_REQUEST_INTERVAL_SEC}
+        };
+
+        auto stopSignalLock = std::unique_lock<std::mutex>{stopSignalLock_};
+        
+        while(true) {
+            cvSignalManager_.wait_for(
+                stopSignalLock, 
+                apiRequestInterval, 
+                [this] () {
+                    return stopReading_;
+                }
+            );
+
+            if (stopReading_) {
+                break;
+            }
+
+            auto latestPrice = getDataRequest();
+            {
+                auto dataLock = std::lock_guard<std::mutex>{dataContainerMutex_};
+                dataContainer_.push_back(latestPrice);
+            }
+        
+        }
+    }
+};
 
 int main() {
     
-    std::vector<double> dataPoints{};
-    std::mutex dataLock{};
-    int stop = 1;
+    auto dataPoints = std::vector<double>{};
+    auto dataLock = std::mutex{};
+    
+    auto readThread = ReadDataThread{dataPoints, dataLock};
+    readThread.startThread();
 
-    auto readThread = std::thread{
-        readData, 
-        std::ref(dataPoints), 
-        std::ref(dataLock), 
-        std::ref(stop)
-    };
+    std::this_thread::sleep_for(std::chrono::seconds{20});
 
-    std::cout << "hi from main\n";
-    std::cin >> stop;
+    readThread.stopThread();
 
-    readThread.join();
+    for (double curr : dataPoints) {
+        std::cout << curr << '\n';
+    }
+
+
 }
